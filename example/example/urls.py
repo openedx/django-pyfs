466a46b006b20470767774a56cecdeab29e1076f
salmannawaz <salman.nawaz@arbisoft.com>
2023-07-12 14:56:51 +0500

    Django 4.2 updates (#135)
    * fix: update django 4.2 support
    
    ---------
    
    Co-authored-by: Salman Nawaz <salman.nawaz@A006-01081.local>

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, include, url
+from django.urls import path
+from django.conf.urls import patterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,7 +7,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'sample.views.index', name='index'),
+    path('', 'sample.views.index', name='index'),
     # url(r'^example/', include('example.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
